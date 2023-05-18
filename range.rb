c07a465788dde47f472462fb9e3137084e66144c
ironman <stark@ironmans-MacBook-Air.local>
2023-05-18 21:02:41 +0530

    first commit

---
+++ range.rb
@@ -0,0 +1,15 @@
+# Ruby Tutorial - Object Method Parameters
+
+# A method may take zero or more parameters as input. To demonstrate this, we look at the asserts we use on HackerRank. Sometimes, we have
+# to check whether a given number a is within the range b and c (where b  c, and both inclusive ).
+
+# Three variables a, b, and c are already defined. Your task is to write code that checks whether a is within the range of b and c by
+# calling the method range? (which we have defined for you as a method for this example) on a and passing b and c as arguments.
+
+# write your code here
+a=4
+b=5
+c=10
+# check whether a is between b to c
+isbetween= a>=b && a<=c
+puts(isbetween)
